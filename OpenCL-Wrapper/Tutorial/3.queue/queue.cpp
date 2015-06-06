3c7b03d774eb47199c92f17a7d95aee9d1ad0011
SeminarOpenCLGRP7 <mirko.berger@tuhh.de>
2015-06-06 15:55:12 +0200

    Random Testing II

--- OpenCL-Wrapper/Tutorial/3.queue/queue.cpp
+++ OpenCL-Wrapper/Tutorial/3.queue/queue.cpp
@@ -30,7 +30,7 @@ int main()
     // create command queue.
     ocl::Queue queue1(context1, device);
     ocl::Queue queue2(context2, device);
-    ocl::Queue queue3(context2, device);
+    ocl::Queue queue4(context2, device);
 
     // can set an active queue.
     // note that there can only be one active queue within a context.
@@ -41,7 +41,7 @@ int main()
     context1.activeQueue() == queue1 ? s = "active!" : s = "inactive!";
     std::cout << "Queue 1 is on context 1 " << s << std::endl;
 
-    context2.activeQueue() == queue3 ? s = "active!" : s = "inactive!";
+    context2.activeQueue() == queue4 ? s = "active!" : s = "inactive!";
     std::cout << "Queue 2 is on context 2 " << s << std::endl;
 
     // create command queue which executes the commands out-of-order.
